97e23053f99c78cc39fc4daef9d2fde2ea9eab04
zurda <m.weizman@gmail.com>
2020-01-12 22:07:47 +0000

    Added story addon: display story

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,22 @@
+var path = require('path');
+
 module.exports = {
   stories: ['../src/**/*.stories.js', '../src/**/*.stories.tsx'],
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
+    {
+      name: '@storybook/addon-storysource',
+      options: {
+        rule: {
+          // test: [/\.stories\.jsx?$/], This is default
+          include: [path.resolve(__dirname, '../src/stories')], // You can specify directories
+        },
+        loaderOptions: {
+          prettierConfig: { printWidth: 80, singleQuote: false },
+        },
+      },
+    },
     '@storybook/addon-links',
   ],
 };
