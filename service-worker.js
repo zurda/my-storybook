46955cac84b2505d1a59ee89e29ad0a7fb50de5d
zurda <m.weizman@gmail.com>
2020-01-16 00:11:03 +0000

    Deploying to gh-pages from  8af633753d1bd5b4006b82d6c3fe8dbb213ea537

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.784b026ff2d5a72d8ad74747a6380495.js"
+  "precache-manifest.9cad30a70aa9e99d8f53fdd3e699ca86.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-storybook/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
