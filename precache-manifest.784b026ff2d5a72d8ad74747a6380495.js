b97792706d099a2dfaae62b677a9a90ee8a7cbf6
zurda <m.weizman@gmail.com>
2020-01-16 00:09:52 +0000

    Deploying to gh-pages from  8af633753d1bd5b4006b82d6c3fe8dbb213ea537

---
+++ precache-manifest.784b026ff2d5a72d8ad74747a6380495.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "877eece23ab1ae1b28719def67e1c7c2",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.1bb2ff8d0640d91716b2.bundle.js"
+  },
+  {
+    "url": "runtime~main.1bb2ff8d0640d91716b2.bundle.js"
+  },
+  {
+    "revision": "aa86276afbc0abfc19c5fb05e9fb4c91",
+    "url": "static/media/down_arrow.aa86276a.svg"
+  },
+  {
+    "url": "vendors~main.1bb2ff8d0640d91716b2.bundle.js"
+  },
+  {
+    "url": "vendors~main.1bb2ff8d0640d91716b2.bundle.js.LICENSE"
+  }
+]);
