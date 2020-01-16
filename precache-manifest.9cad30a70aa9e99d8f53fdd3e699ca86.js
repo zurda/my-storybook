b01ff70d13e28b7233e663f4f7aa5d03ac127ccb
zurda <m.weizman@gmail.com>
2020-01-16 00:09:35 +0000

    Deploying to gh-pages from  8af633753d1bd5b4006b82d6c3fe8dbb213ea537

---
+++ precache-manifest.9cad30a70aa9e99d8f53fdd3e699ca86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b278a6de08428cf180940f3c5a9b2f2",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.d51b837ef4030b0805bc.bundle.js"
+  },
+  {
+    "url": "runtime~main.d51b837ef4030b0805bc.bundle.js"
+  },
+  {
+    "revision": "aa86276afbc0abfc19c5fb05e9fb4c91",
+    "url": "static/media/down_arrow.aa86276a.svg"
+  },
+  {
+    "url": "vendors~main.d51b837ef4030b0805bc.bundle.js"
+  },
+  {
+    "url": "vendors~main.d51b837ef4030b0805bc.bundle.js.LICENSE"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.585c2bfdd9a15ccedad934ec19293ee8.js"
+  "precache-manifest.9cad30a70aa9e99d8f53fdd3e699ca86.js"
 );
 
 self.addEventListener('message', (event) => {
